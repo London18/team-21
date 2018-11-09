96d2f717343f48ae69bfdd228ff1b93c5b9eef8e
Yuriy Maksymets <iurii.maksymets@gmail.com>
2018-11-09 23:37:45 +0000

    Remember me option.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,28 +5,32 @@ import Survey from "./Components/Survey";
 import Login from "./Components/Login";
 import Dashboard from "./Components/Dashboard";
 
-const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/login/">Login</Link>
-          </li>
-          <li>
-            <Link to="/survey/">Survey</Link>
-          </li>
-        </ul>
-      </nav>
+const App = () => {
+  let loggedInBefore = localStorage.getItem("loggedIn");
+  let Home = loggedInBefore === "true" ? Dashboard : Login;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+            <li>
+              <Link to="/survey/">Survey</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Route path="/" exact component={Dashboard} />
-      <Route path="/login/" exact component={Login} />
-      <Route path="/survey/" component={Survey} />
-    </div>
-  </Router>
-);
+        <Route path="/" exact component={Home} />
+        <Route path="/login/" exact component={Login} />
+        <Route path="/survey/" component={Survey} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;

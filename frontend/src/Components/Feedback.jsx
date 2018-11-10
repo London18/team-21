3d0b583084e1722b57103e1fbe4a8616492664a9
JamesCraster <jamesvaughancraster@gmail.com>
2018-11-10 07:59:38 +0000

    your tree

--- frontend/src/Components/Feedback.jsx
+++ frontend/src/Components/Feedback.jsx
@@ -9,7 +9,7 @@ class Feedback extends React.Component {
           You've contributed x hours to autism research. You've helped us to
           improve the lives of people with autism all around the world.{" "}
         </p>
-        <b>You've earned xyz points</b>
+        <b>You've earned xyz points, your garden is growing!</b>
       </div>
     );
   }

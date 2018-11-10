47dc1d6ffc553f90755e2a9ab43e57ddf22c2a95
wurkata <yavoredipov@gmail.com>
2018-11-10 04:45:30 +0000

    Fixed untracked files

--- backnd/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\projects\cfg\team-21\backnd\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- backnd/autistica/surveys/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-        path('', views.surveys, name='surveys'),
-        path('<str:survey>', views.survey_questions, name='survey_questions'),
-]

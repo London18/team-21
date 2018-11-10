2c0cf3f8cd186e5ba1fc87bd4cb321c37624dae7
wurkata <yavoredipov@gmail.com>
2018-11-10 07:57:49 +0000

    Able to push data to db

--- backnd/autistica/surveys/urls.py
+++ backnd/autistica/surveys/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
         path('', views.surveys, name='surveys'),
-        path('<str:survey>', views.survey_questions, name='survey_questions'),
+        path('<str:forSurvey>', views.survey_questions, name='survey_questions'),
 ]

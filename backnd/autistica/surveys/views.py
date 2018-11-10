9d8a42e383b6a3a542c9132555eb02cda74d0c5f
wurkata <yavoredipov@gmail.com>
2018-11-10 02:53:27 +0000

    Update urls

--- backnd/autistica/surveys/views.py
+++ backnd/autistica/surveys/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
-from surveys.models import SurveyQuestions
+from surveys.models import Survey, SurveyQuestions
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Testing...")
 
-def survey(request, sCategory='all', sTitle='1'):
-    json_questions = fetchQuestions()
+def survey(request, survey='all'):
+    json_questions = fetchQuestions(survey)
     print(json_questions)
     return JsonResponse(json_questions, safe=False)
 
@@ -17,14 +17,25 @@ def dashboard(request):
     return HttpResponse("Dashboard")
 
 def fetchQuestions(forSurvey='all'):
+    query = []
     if forSurvey=='all':
         query = SurveyQuestions.objects.all()
-        objList = []
-        for data in query:
-            objList.append({'qText': data.question.question_text, 'qType': data.question.qtype})
+    else:
+        surveyId = -1
+        try:
+            surveyId = Survey.objects.get(title=forSurvey).id
+        except Survey.DoesNotExist:
+            surveyId = -1
 
-        print(objList)
-        json_query = json.dumps(objList)
-        return json_query
+        if surveyId >= 0:  
+            query = SurveyQuestions.objects.filter(survey=surveyId)
+
+    objList = []
+    
+    for data in query:
+        objList.append({'qText': data.question.question_text, 'qType': data.question.qtype})
+
+    json_query = json.dumps(objList)
+    return json_query
 
 
